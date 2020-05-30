6f744da42109076810f9af3ab60a7653201ac24b
Shutkin Yura <yshutkin@infra-lab.xyz>
2020-05-30 19:42:09 +0500

    Fix module name at go.mod; add show time now with ntp query result

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
+func HelloNow() {
+	timeNow := time.Now().Round(time.Second)
+	fmt.Printf("current time: %v\n", timeNow)
+
+	exactTime, err := ntp.Time("pool.ntp.org")
+	if err != nil {
+		log.Fatalf("An error occurred. Error is: %v", err)
+	}
+
+	fmt.Printf("exact time: %v\n", exactTime.Round(time.Second))
+}
+
 func main() {
-	// Place your code here
+	HelloNow()
 }
