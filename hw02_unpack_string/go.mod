876538888b6eb604b2f79fae82099f79f3c866d3
Shutkin Yura <yshutkin@infra-lab.xyz>
2020-07-27 11:04:30 +0500

    Add unpack string function

--- hw02_unpack_string/go.mod
+++ hw02_unpack_string/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw02_unpack_string
+module github.com/yura-shutkin/home_work_go_2020_05/hw02_unpack_string
 
 go 1.14
 
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,79 @@ package hw02_unpack_string //nolint:golint,stylecheck
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here
-	return "", nil
+//var ErrFirstCharIsNum = errors.New("first char can not be numeric")
+//var ErrUnknowChar = errors.New("not a letter, not a numeric, not an escape char")
+var EscapeCharList = []rune{
+	'\\',
+	'*',
+	':',
+}
+
+func IsInList(myRune rune, listRunes []rune) bool {
+	flag := false
+	for _, val := range listRunes {
+		if val == myRune {
+			flag = true
+			break
+		}
+	}
+	return flag
+}
+
+func Unpack(msg string) (string, error) {
+	var buf string
+	var isPrevEsc bool
+	resultString := ""
+
+	for id, val := range msg {
+		isPrevEsc = false
+		// Check is first Char is Numeric
+		if id == 0 {
+			if unicode.IsDigit(val) {
+				return "", ErrInvalidString
+			}
+			buf = buf + string(val)
+			continue
+		}
+		// Check if lower letter
+		if unicode.IsLetter(val) {
+			if len(buf) != 0 {
+				resultString = resultString + buf
+				buf = string(val)
+				continue
+			}
+			buf = buf + string(val)
+			continue
+		}
+		// Check if escape char
+		if IsInList(val, EscapeCharList) {
+			buf = buf + string(val)
+			continue
+		}
+		if unicode.IsDigit(val) {
+			if isPrevEsc {
+				buf = buf + string(val)
+				continue
+			}
+			if len(buf) == 0 {
+				return "", ErrInvalidString
+			}
+			resultString = resultString + strings.Repeat(buf, int(val-'0'))
+			buf = ""
+			continue
+		}
+		// Unknown char
+		return "", ErrInvalidString
+	}
+
+	if len(buf) != 0 {
+		resultString = resultString + buf
+	}
+	return resultString, nil
 }
