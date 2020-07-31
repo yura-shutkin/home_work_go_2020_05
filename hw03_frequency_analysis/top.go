b3f46eafe8e41d566c697b40afee575a4b32421f
Shutkin Yura <yshutkin@infra-lab.xyz>
2020-07-31 09:56:14 +0500

    Tests are passed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,54 @@
 package hw03_frequency_analysis //nolint:golint,stylecheck
+import (
+	"strings"
+)
 
-func Top10(_ string) []string {
-	// Place your code here
-	return nil
+func MassReplace(str, search, replace string) string {
+	for {
+		if strings.Contains(str, search) {
+			str = strings.ReplaceAll(str, search, replace)
+		} else {
+			return str
+		}
+	}
+}
+
+func Top10(msg string) []string {
+	if len(msg) == 0 {
+		return nil
+	}
+	ratings := make(map[string]int)
+	msg = MassReplace(msg, "\t", " ")
+	msg = MassReplace(msg, "\n", " ")
+	msg = MassReplace(msg, "  ", " ")
+
+	raw := strings.Split(msg, " ")
+
+	for _, val := range raw {
+		_, ok := ratings[val]
+		if !ok {
+			ratings[val] = 1
+		} else {
+			ratings[val] += 1
+		}
+	}
+
+	var top []string
+	for lim := 10; lim > 0; lim -= 1 {
+		max := 0
+		for _, val := range ratings {
+			if val > max {
+				max = val
+			}
+		}
+		for key, val := range ratings {
+			if val == max {
+				top = append(top, key)
+				delete(ratings, key)
+				break
+			}
+		}
+	}
+
+	return top
 }
